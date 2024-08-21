ef3c83c5c57e2d13c48173ed7582a2072c269afc
Bagas <108858099+VampXDH@users.noreply.github.com>
2024-08-22 05:05:47 +0700

    Update bot.go

--- bot.go
+++ bot.go
@@ -2,7 +2,6 @@ package telegrambot
 
 import (
 	"bytes"
-	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -16,12 +15,11 @@ import (
 	"time"
 )
 
-// Bot struct represents a Telegram Bot
 type Bot struct {
-	Token       string
-	Client      *http.Client
-	commands    map[string]func(chatID int64) string
-	mu          sync.Mutex
+	Token    string
+	Client   *http.Client
+	commands map[string]func(chatID int64) string
+	mu       sync.Mutex
 }
 
 // NewBot creates a new Telegram Bot instance
@@ -179,4 +177,66 @@ func (b *Bot) SendFile(chatID int64, filePath string, caption string) error {
 	}
 
 	return nil
+}
+
+// Update represents an update from Telegram
+type Update struct {
+	UpdateID int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+// Message represents a message from Telegram
+type Message struct {
+	MessageID int      `json:"message_id"`
+	From      User     `json:"from"`
+	Chat      Chat     `json:"chat"`
+	Date      int      `json:"date"`
+	Text      string   `json:"text"`
+	Document  Document `json:"document"`  // Field untuk dokumen yang dikirim
+}
+
+// Document represents a document sent to the bot
+type Document struct {
+	FileID   string `json:"file_id"`
+	FileName string `json:"file_name"`
+	FileSize int    `json:"file_size"`
+}
+
+// User represents a user on Telegram
+type User struct {
+	ID           int    `json:"id"`
+	IsBot        bool   `json:"is_bot"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Username     string `json:"username"`
+	LanguageCode string `json:"language_code"`
+}
+
+// Chat represents a chat on Telegram
+type Chat struct {
+	ID        int64  `json:"id"`
+	Type      string `json:"type"`
+	Title     string `json:"title"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+// UpdateResponse represents the response from Telegram getUpdates method
+type UpdateResponse struct {
+	Ok     bool     `json:"ok"`
+	Result []Update `json:"result"`
+}
+
+// FileResponse represents the response from Telegram getFile method
+type FileResponse struct {
+	Ok     bool   `json:"ok"`
+	Result File   `json:"result"`
+}
+
+// File represents the file information from Telegram getFile response
+type File struct {
+	FileID   string `json:"file_id"`
+	FilePath string `json:"file_path"`
+}
 }
